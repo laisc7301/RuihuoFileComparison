bb1d40bc0c2f8b53b9ae0286330d9b4ae090962c
laisc(home) <laisc7301@gmail.com>
2024-11-18 16:33:46 +0800

    n

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,16 +16,26 @@ MainWindow::~MainWindow()
 }
 
 
+
+
+
+
 void MainWindow::on_startComparingButton_clicked()
 {
     QString pathA = ui->pathALineEdit->text();
     QString pathB = ui->pathBLineEdit->text();
 
-    pathA = R"(C:\工作区\Workspace\test.txt)";
-    pathB = R"(C:\工作区\Workspace\test2.txt)";
+    //pathA = R"(C:\工作区\Workspace\test.txt)";
+    //pathB = R"(C:\工作区\Workspace\test2.txt)";
 
     //ui->pathALineEdit->setText(pathA);
     QFile file(pathA);
+
+    if (!file.exists()) {
+        qDebug() << "文件不存在:" << pathA;
+    }else {
+        qDebug() << "找到文件:" << pathA;
+    }
 
 }
 
